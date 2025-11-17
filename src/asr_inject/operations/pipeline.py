a0673a005e906799266e34aa3e8a4e050e77af28
Ali Taqi <alitaqi94.developer@gmail.com>
2025-11-17 21:36:05 +0400

    `read_yaml()` added

--- src/asr_inject/operations/pipeline.py
+++ src/asr_inject/operations/pipeline.py
@@ -1,2 +1,13 @@
 """ Licensed under the same terms as described in the main 
 licensing script of this repository. """
+
+import yaml
+from pathlib import Path
+from typing import Any
+
+
+def read_yaml(dir: Path) -> dict[str, Any]:
+    """
+    """
+    with open(f"{dir}", 'r') as file:
+        return yaml.safe_load(file)
