ce2dd55e2b243db6f2f3ea1003360d0b13b1b080
Ali Taqi <alitaqi94.developer@gmail.com>
2025-11-29 11:17:41 +0400

    progress

--- src/asr_inject/operations/visualise.py
+++ src/asr_inject/operations/visualise.py
@@ -52,3 +52,35 @@ def plot_2d(
     plt.ylabel("mass (kg)")
     plt.savefig(f"{outdir / f"{filename}.png"}")
     plt.close()
+
+    # solute
+    solute_mass = results["moles"][:, 2:] * (
+        config["solution_characteristics"]["Mr_solute"] /
+        k_TO_SI
+    )
+
+    plt.plot(
+        (
+            np.arange(config["n_steps"]) *
+            config["step_size"] / DAY_TO_SEC
+        ),
+        solute_mass[:, 0],
+        label="fresh segment"
+    )
+
+    plt.plot(
+        (
+            np.arange(config["n_steps"]) *
+            config["step_size"] / DAY_TO_SEC
+        ),
+        solute_mass[:, 1],
+        label="saline segment"
+    )
+
+    filename = "solute_moles"
+    plt.legend(loc="best")
+    plt.title(filename)
+    plt.xlabel("time (days)")
+    plt.ylabel("mass (kg)")
+    plt.savefig(f"{outdir / f"{filename}.png"}")
+    plt.close()
