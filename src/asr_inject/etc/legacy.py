ee0cb8c610680ba0005823f5d96dd5a996a2929e
Ali Taqi <alitaqi94.developer@gmail.com>
2026-09-21 20:49:50 +0400

    minor improvements

--- src/asr_inject/etc/legacy.py
+++ src/asr_inject/etc/legacy.py
@@ -1,7 +1,6 @@
 """ Licensed under the same terms as described in the main 
 licensing script of this repository. """
 
-
             # average diffusion coefficients
             #water_concentration_mean = np.mean([
             #    water_concentration_fresh,
